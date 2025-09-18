53b868ebbf2f540c906693067d00f66f9298d8ba
Mateusz Górski <67629294+Ziomiu@users.noreply.github.com>
2025-09-18 16:04:29 +0200

    Enpoint for students
    - Added endpoint for getting all students

--- backend/src/main/java/com/example/tutorApp/repository/UserRepository.java
+++ backend/src/main/java/com/example/tutorApp/repository/UserRepository.java
@@ -1,13 +1,17 @@
 package com.example.tutorApp.repository;
 
 import com.example.tutorApp.entity.AppUser;
+import com.example.tutorApp.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, UUID> {
     Optional<AppUser> findByEmail(String email);
+
+    List<AppUser> findByUserRoleEquals(UserRole userRole);
 }
